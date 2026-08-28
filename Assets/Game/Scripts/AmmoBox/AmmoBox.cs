7a3c0c16f058e7dfbe890314b2a2fd542274d990
vipol0 <vipolo452@gmail.com>
2026-08-28 19:27:30 +0500

    Day 4 (0.0.1.5)

--- Assets/Game/Scripts/AmmoBox/AmmoBox.cs
+++ Assets/Game/Scripts/AmmoBox/AmmoBox.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using System;
 
 public class AmmoBox : MonoBehaviour
 {
     [SerializeField] private int ammo = 5;
     [SerializeField] private WeaponType weaponType;
 
+    public event Action GetAmmo; 
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerWeaponSystem>()?.Resupply(weaponType, ammo);
+            GetAmmo?.Invoke();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Game/Scripts/ItemSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ItemSpawner : MonoBehaviour
+{
+    [Serializable]
+    public struct AmmoSlot
+    {
+        public WeaponType type;
+        public GameObject prefab;
+    }
+
+    [SerializeField] private List<AmmoSlot> ammoSlots = new();
+    [SerializeField] private float spawnCoolDown = 1f;
+
+    private GameObject currentItem;
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            if (currentItem == null && ammoSlots.Count > 0)
+            {
+                var index = Random.Range(0, ammoSlots.Count);
+                currentItem = Instantiate(ammoSlots[index].prefab, transform.position, Quaternion.identity);
+            }
+
+            yield return new WaitForSeconds(spawnCoolDown);
+        }
+    }
+}
